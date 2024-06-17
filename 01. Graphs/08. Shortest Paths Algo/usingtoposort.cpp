36a0f09e6f91639caa0f45bb930cc9ad22331a1d
Prakhar Singh <105231046+trojan21@users.noreply.github.com>
2024-06-18 01:13:37 +0530

    Create usingtoposort.cpp

---
+++ 01. Graphs/08. Shortest Paths Algo/usingtoposort.cpp
@@ -0,0 +1,53 @@
+//use topo sort
+// and then relax edges
+// this is for directed graph
+// why topo sort: using topo sort  ensures that after reaching a node u have computed all the nodes preceding it
+void topoSort(int node, vector < pair < int, int >> adj[],
+      int vis[], stack < int > & st) {
+      //This is the function to implement Topological sort. 
+      vis[node] = 1;
+      for (auto it: adj[node]) {
+        int v = it.first;
+        if (!vis[v]) {
+          topoSort(v, adj, vis, st);
+        }
+      }
+      st.push(node);
+    }
+vector < int > shortestPath(int N, int M, vector < vector < int >> & edges) {
+
+      //We create a graph first in the form of an adjacency list.
+      vector < pair < int, int >> adj[N];
+      for (int i = 0; i < M; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int wt = edges[i][2];
+        adj[u].push_back({v, wt}); 
+      }
+      vector<int> vis(N, 0);
+      vector<int> dis(N, 1e9);
+      //now we will do relaxation of edges
+dist[0] = 0;
+      stack < int > st;
+      for (int i = 0; i < N; i++) {
+        if (!vis[i]) {
+          topoSort(i, adj, vis, st);
+        }
+      }
+      while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+
+        for (auto it: adj[node]) {
+          int v = it.first;
+          int wt = it.second;
+
+          if (dist[node] + wt < dist[v]) {
+            dist[v] = wt + dist[node];
+          }
+        }
+      }
+      for (int i = 0; i < N; i++) {
+        if (dist[i] == 1e9) dist[i] = -1;
+      }
+      return dist;
