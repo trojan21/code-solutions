794167dcec2fcc0f8a8a48d118c0577204f6b690
Prakhar Singh <105231046+trojan21@users.noreply.github.com>
2024-06-18 01:15:04 +0530

    Update 1. Dijkstras.cpp

--- 01. Graphs/08. Shortest Paths Algo/1. Dijkstras.cpp
+++ 01. Graphs/08. Shortest Paths Algo/1. Dijkstras.cpp
@@ -55,3 +55,108 @@ public:
         return ans==LONG_MAX?-1:ans;
     }
 };
+
+//class Solution
+{
+public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex S.
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+
+        // Create a priority queue for storing the nodes as a pair {dist,node}
+        // where dist is the distance from source to the node. 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        // Initialising distTo list with a large number to
+        // indicate the nodes are unvisited initially.
+        // This list contains distance from source to the nodes.
+        vector<int> distTo(V, INT_MAX);
+
+        // Source initialised with dist=0.
+        distTo[S] = 0;
+        pq.push({0, S});
+
+        // Now, pop the minimum distance node first from the min-heap
+        // and traverse for all its adjacent nodes.
+        while (!pq.empty())
+        {
+            int node = pq.top().second;
+            int dis = pq.top().first;
+            pq.pop();
+
+            // Check for all adjacent nodes of the popped out
+            // element whether the prev dist is larger than current or not.
+            for (auto it : adj[node])
+            {
+                int v = it[0];
+                int w = it[1];
+                if (dis + w < distTo[v])
+                {
+                    distTo[v] = dis + w;
+    
+                    // If current distance is smaller,
+                    // push it into the queue.
+                    pq.push({dis + w, v});
+                }
+            }
+        }
+        // Return the list containing shortest distances
+        // from source to all the nodes.
+        return distTo;
+    }
+};
+
+
+//STRIVERS SOLUTION
+
+class Solution
+{
+public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex S.
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+
+        // Create a priority queue for storing the nodes as a pair {dist,node}
+        // where dist is the distance from source to the node. 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        // Initialising distTo list with a large number to
+        // indicate the nodes are unvisited initially.
+        // This list contains distance from source to the nodes.
+        vector<int> distTo(V, INT_MAX);
+
+        // Source initialised with dist=0.
+        distTo[S] = 0;
+        pq.push({0, S});
+
+        // Now, pop the minimum distance node first from the min-heap
+        // and traverse for all its adjacent nodes.
+        while (!pq.empty())
+        {
+            int node = pq.top().second;
+            int dis = pq.top().first;
+            pq.pop();
+
+            // Check for all adjacent nodes of the popped out
+            // element whether the prev dist is larger than current or not.
+            for (auto it : adj[node])
+            {
+                int v = it[0];
+                int w = it[1];
+                if (dis + w < distTo[v])
+                {
+                    distTo[v] = dis + w;
+    
+                    // If current distance is smaller,
+                    // push it into the queue.
+                    pq.push({dis + w, v});
+                }
+            }
+        }
+        // Return the list containing shortest distances
+        // from source to all the nodes.
+        return distTo;
+    }
+};
