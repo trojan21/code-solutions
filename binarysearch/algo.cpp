cd98c6e5c5db0cdbb79862784fa310d78440fb9d
Prakhar Singh <105231046+trojan21@users.noreply.github.com>
2024-06-17 18:50:00 +0530

    Update algo.cpp

--- binarysearch/algo.cpp
+++ binarysearch/algo.cpp
@@ -5,3 +5,6 @@ while(low<=high)
   else if(ele > mid) high = mid - 1;
   else low = mid+1;
 }
+// if  you are searching for an element and that key is not in the array then after the loop low will be at its right neighbour and high will be at its left neighbout
+//1 3 5 10 15 now you search for 12 after the loop high is at 10 and low is at 15
+//another variation is when the array is not sorted you need a certain criteria to reject one of the half.
